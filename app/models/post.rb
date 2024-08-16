318509d233edfa4a63e888bb7add7943cad47e41
lnpotter <lucasnoalpottergithub@gmail.com>
2024-08-16 17:29:13 -0300

    feat(posts): add SEO-friendly URLs with slugs - Added  column to the posts table. - Automatically generate slugs from the post title using the  method. - Updated routes to use slugs instead of IDs. - Modified  to find posts by their slug.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
-    has_one_attached :image
-end
+    before_save :generate_slug
+  
+    validates :title, presence: true
+  
+    def generate_slug
+      self.slug = title.parameterize if title
+    end
+  
+    def to_param
+      slug
+    end
+  end
   
